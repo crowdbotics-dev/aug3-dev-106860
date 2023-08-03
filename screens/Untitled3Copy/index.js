974895ed41c359637831987580fb037699b5aa09
Mithilesh Tiwari <Mithilesh@crowdbotics.com>
2023-08-03 17:32:44 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

---
+++ screens/Untitled3Copy/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
+
+const Untitled3Copy = () => {
+  return <SafeAreaView style={styles.safeArea} __uuid__="397e0070-f7a3-40b4-ba5b-4d0a4f208bd9">
+        <ScrollView contentContainerStyle={{
+      backgroundColor: '#f0f0f1',
+      padding: 10,
+      position: 'relative',
+      flex: 1
+    }} __uuid__="f911fb0f-08fc-4e76-bfeb-ceeebafb30c7"></ScrollView>
+    </SafeAreaView>;
+};
+
+const styles = StyleSheet.create({
+  safeArea: {
+    height: '100%'
+  }
+});
+export default Untitled3Copy;
---
+++ screens/Untitled3Copy1/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
+
+const Untitled3Copy1 = () => {
+  return <SafeAreaView style={styles.safeArea} __uuid__="03dd21c2-ed1c-43b3-b23c-1f1cb03bc650">
+        <ScrollView contentContainerStyle={{
+      backgroundColor: '#f0f0f1',
+      padding: 10,
+      position: 'relative',
+      flex: 1
+    }} __uuid__="eddc5cfd-513f-4ab5-bfeb-5bd200af5ed5"></ScrollView>
+    </SafeAreaView>;
+};
+
+const styles = StyleSheet.create({
+  safeArea: {
+    height: '100%'
+  }
+});
+export default Untitled3Copy1;
